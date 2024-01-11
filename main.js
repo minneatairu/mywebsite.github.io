a79d3ab623620d122b650aecaf82447c99374496
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-01-11 11:33:57 -0500

    V

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
+// CAROUSEL: HOME
 document.addEventListener("DOMContentLoaded", function () {
     const prevButton = document.querySelector('.prev');
     const nextButton = document.querySelector('.next');
     const carousel = document.querySelector('.carousel');
     let scrollAmount = 0;
 
+    const getScrollWidth = () => {
+        // Get the width of a single item (100% on mobile)
+        return carousel.offsetWidth / (window.innerWidth <= 600 ? 1 : 3);
+    };
+
     nextButton.addEventListener('click', () => {
+        scrollAmount += getScrollWidth();
         carousel.scrollTo({
             top: 0,
-            left: (scrollAmount += carousel.offsetWidth / 3),
+            left: scrollAmount,
             behavior: 'smooth'
         });
     });
 
     prevButton.addEventListener('click', () => {
+        scrollAmount -= getScrollWidth();
         carousel.scrollTo({
             top: 0,
-            left: (scrollAmount -= carousel.offsetWidth / 3),
+            left: scrollAmount,
             behavior: 'smooth'
         });
     });
 });
+
+
+//RADIO: EVENTS
 
 
 let lastSelectedRadio = null;

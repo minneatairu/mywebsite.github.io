01db121b56e9ce18d9ab69f93cd568f76a611bf1
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-01-10 22:26:51 -0500

    v

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    const carousel = document.querySelector('.carousel');
+    let scrollAmount = 0;
+
+    nextButton.addEventListener('click', () => {
+        carousel.scrollTo({
+            top: 0,
+            left: (scrollAmount += carousel.offsetWidth / 3),
+            behavior: 'smooth'
+        });
+    });
+
+    prevButton.addEventListener('click', () => {
+        carousel.scrollTo({
+            top: 0,
+            left: (scrollAmount -= carousel.offsetWidth / 3),
+            behavior: 'smooth'
+        });
+    });
+});

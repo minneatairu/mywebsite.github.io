21b77a1dc41a77b8950f8e22fe8e2ccb48af3ba1
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-01-11 21:57:42 -0500

    <div id="header-container"></div>

--- main.js
+++ main.js
@@ -171,7 +171,7 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('header.html')
       .then(response => response.text())
       .then(data => {
-        document.body.insertAdjacentHTML('afterbegin', data);
+        document.getElementById('header-container').innerHTML = data;
       });
   });
   

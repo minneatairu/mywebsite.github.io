906b96470bd4af92bf3d7de2f35e15cbfaf8557d
hasnainhashmi <59309374+hasnainhashmi@users.noreply.github.com>
2024-01-17 12:24:24 +0500

    Header

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 //header
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('header.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('header-container').innerHTML = data;
-      });
+fetch('header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header-container').innerHTML = data;
   });
 
 // CAROUSEL: HOME

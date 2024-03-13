f4f6e0ac854873feb01717958dbb2039b959eea4
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-03-13 00:04:13 -0400

    d

--- main.js
+++ main.js
@@ -258,3 +258,32 @@ document.addEventListener('DOMContentLoaded', function () {
         prev.addEventListener("click", prevSlide);
     });
     
+    //list modal
+
+    document.addEventListener("DOMContentLoaded", function() {
+        // Get the modal
+        var modal = document.getElementById('listedModal');
+      
+        // Get the element that opens the modal
+        var btn = document.getElementById('viewByList');
+      
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+      
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+          modal.style.display = "block";
+        }
+      
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+      
+        // Also close the modal if the user clicks anywhere outside of it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+      });
